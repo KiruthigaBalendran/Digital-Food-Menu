c8a618976961b1ffbd5ac791213a7b99c94c7c06
KiruthigaBalendran <kiruthiga03@gmail.com>
2017-12-23 00:24:05 +0530

    commented codes removed

--- index.php
+++ index.php
@@ -67,15 +67,6 @@
 
     <div id="header">
         <div class="bg-overlay"></div>
-        <!-- <div class="center text-center">
-            <div class="banner">
-                <h1 class="">Foodicious</h1>
-            </div>
-            <div class="subtitle"><h4>AWESOME RESTAURANT THEME</h4></div>
-        </div>
-        <div class="bottom text-center">
-            <a id="scrollDownArrow" href="#"><i class="fa fa-chevron-down"></i></a>
-        </div> -->
         <div class="container">  
           <div id="myCarousel" class="carousel slide" data-ride="carousel">
             <!-- Indicators -->
